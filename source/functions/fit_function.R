878551a13fb4d93a0803a427b4e38febb14b0291
Yang <yang.xiao@kaust.edu.sa>
2025-08-11 17:44:09 +0300

    application
    some updates on application BR

--- source/functions/fit_function.R
+++ source/functions/fit_function.R
@@ -1,14 +1,154 @@
+library(dplyr)
+library(zoo)
+library(lubridate)
+library(readr)
+
+# run_moving_window <- function(root_dir_infodengue, root_dir_GT,
+#                               state, full_start, full_end,
+#                               window_length = 8, step_size = 1,
+#                               D, hypers, model_file,
+#                               model_name = c("ARABM", "ARABMwGT"),
+#                               E , bases_s, S,
+#                               mode = c("fixed", "expanding"),
+#                               iter_sampling = 10000, iter_warmup = 5000,
+#                               chains = 3, thin = 1,
+#                               posterior_draws_path
+#                               ) {
+#   mode <- match.arg(mode)
+# 
+#   # Convert full_start and full_end to Dates. Assume full_start and full_end are given in "YYYYWW"
+#   start_date <- get_date(
+#     year = substr(full_start, 1, 4),
+#     week = substr(full_start, 5, 6)
+#   )
+#   end_date <- get_date(
+#     year = substr(full_end, 1, 4),
+#     week = substr(full_end, 5, 6)
+#   )
+# 
+#   if (end_date < start_date) {
+#     stop("End date cannot be earlier than start date.")
+#   }
+# 
+#   # Generate a sequence of dates (weekly)
+#   all_eweeks <- seq(from = full_start, to = full_end, by = 1)
+#   all_dates <- seq(from = start_date, to = end_date, by = "week")
+#   total_weeks <- length(all_dates)
+# 
+#   window_list <- list()
+#   if (mode == "fixed") {
+#     # Sliding windows of fixed length
+#     for (i in seq(1+step_size, total_weeks - window_length + 1, by = step_size)) {
+#       window_list[[length(window_list) + 1]] <- list(
+#         start_date = all_dates[i],
+#         end_date = all_dates[i + window_length - 1]
+#       )
+#     }
+#   } else if (mode == "expanding") {
+#     # Expanding window: start is fixed; end increases in steps
+#     for (i in seq(1+step_size, total_weeks, by = step_size)) {
+#       window_list[[length(window_list) + 1]] <- list(
+#         start_date = start_date,
+#         end_date = all_dates[i]
+#       )
+#     }
+#   }
+# 
+#   # Compile Stan model
+#   model <- cmdstan_model(model_file)
+#   results <- list()
+# 
+#   # Loop over each window
+#   for (win in seq_along(window_list)) {
+#     win_start <- window_list[[win]]$start_date
+#     win_end <- window_list[[win]]$end_date
+# 
+#     # Convert window start and end to EW codes (for get_infodengue_data)
+#     win_start_ew <- date_to_ew(win_start)
+#     win_end_ew <- date_to_ew(win_end)
+# 
+#     # Get delay matrix via your existing function (assumes it returns a list with element named by state)
+#     window_data <- get_infodengue_data(root_dir_infodengue = root_dir_infodengue,
+#                                        states = state,
+#                                        start = win_start_ew,
+#                                        end = win_end_ew,
+#                                        D = D,
+#                                        fill_missing = TRUE)
+#     delay_matrix <- window_data[[state]]
+#     delay_matrix[is.na(delay_matrix)] <- 0
+#     T_window <- nrow(delay_matrix)
+# 
+# 
+# 
+#     # Arrange data for Stan
+#     if(model_name == "ARABMwGT"){
+#       gt <- read.csv(file.path(root_dir_GT,win_end_ew, paste0(state,"_trends.csv")),
+#                stringsAsFactors = FALSE, skip = 2)[,c(1:3)] %>%
+#         setNames(c("date", "dengue", "sintomas_dengue")) %>%
+#         filter(date <= win_end & date >= win_start) %>%
+#         mutate(
+#           across(
+#             .cols = -date,
+#             .fns = ~ as.numeric(ifelse(. == "<1", "0.5", .))
+#           )
+#         ) %>%
+#         select("dengue") %>%
+#         mutate(dengue = 2*(dengue - min(dengue)) /
+#                                      (max(dengue) - min(dengue)) - 1) %>%
+#         as.matrix()
+# 
+# 
+#       stan_data <- c(list(T = T_window, D = D, Y = delay_matrix),
+#                      list(K = 1, x = gt, E = E,
+#                           S=S, bases_s = bases_s[1:T_window, ]),
+#                      hypers)
+#     }else{
+#       stan_data <- c(list(T = T_window, D = D, Y = delay_matrix), hypers)
+#     }
+# 
+# 
+#     # Fit the model
+#     fit <- model$sample(
+#       data = stan_data,
+#       iter_sampling = iter_sampling,
+#       iter_warmup = iter_warmup,
+#       chains = chains,
+#       parallel_chains = chains,
+#       thin = thin,
+#       refresh = 0,
+#       output_dir = posterior_draws_path
+#     )
+# 
+#     # Extract a summary (or any diagnostics you need)
+#     fit_summary <- fit$summary()
+# 
+#     results[[win]] <- list(
+#       window_start = win_start,
+#       window_end = win_end,
+#       fit_summary = fit_summary,
+#       fit_object = fit
+#     )
+# 
+#     cat("Finished window", win, "from", format(win_start, "%Y-%m-%d"),
+#         "to", format(win_end, "%Y-%m-%d"), "\n")
+#   }
+# 
+#   return(results)
+# }
 
 run_moving_window <- function(root_dir_infodengue, root_dir_GT,
                               state, full_start, full_end,
-                              window_length = 8, step_size = 1, 
+                              window_length = 8, step_size = 1,
                               D, hypers, model_file,
                               model_name = c("ARABM", "ARABMwGT"),
+                              E , bases_s, S,
+                              historical_year = 2,
+                              smooth_para_gt = 3,
                               mode = c("fixed", "expanding"),
                               iter_sampling = 10000, iter_warmup = 5000,
                               chains = 3, thin = 1,
                               posterior_draws_path
-                              ) {
+) {
   mode <- match.arg(mode)
   
   # Convert full_start and full_end to Dates. Assume full_start and full_end are given in "YYYYWW"
@@ -33,22 +173,18 @@ run_moving_window <- function(root_dir_infodengue, root_dir_GT,
   window_list <- list()
   if (mode == "fixed") {
     # Sliding windows of fixed length
-    for (i in seq(1, total_weeks - window_length + 1, by = step_size)) {
+    for (i in seq(1+step_size, total_weeks - window_length + 1, by = step_size)) {
       window_list[[length(window_list) + 1]] <- list(
         start_date = all_dates[i],
         end_date = all_dates[i + window_length - 1]
-        # start_ew = all_eweeks[i],
-        # end_ew = all_eweeks[i + window_length - 1]
       )
     }
   } else if (mode == "expanding") {
     # Expanding window: start is fixed; end increases in steps
-    for (i in seq(1, total_weeks, by = step_size)) {
+    for (i in seq(1+step_size, total_weeks, by = step_size)) {
       window_list[[length(window_list) + 1]] <- list(
         start_date = start_date,
         end_date = all_dates[i]
-        # start_ew = start_ew,
-        # end_ew = all_eweeks[i]
       )
     }
   }
@@ -56,11 +192,19 @@ run_moving_window <- function(root_dir_infodengue, root_dir_GT,
   # Compile Stan model
   model <- cmdstan_model(model_file)
   results <- list()
+  
+  weeks_per_year <- 52                        
+  hist_len <- historical_year * weeks_per_year       # historial length
+  # date to decide the historical data
+  ealiest_date <- window_list[[1]]$start_date - weeks(historical_year * 52)
+  ealiest_date_for_smooth <- ealiest_date - weeks(smooth_para_gt-1)
 
   # Loop over each window
   for (win in seq_along(window_list)) {
     win_start <- window_list[[win]]$start_date
     win_end <- window_list[[win]]$end_date
+    
+    if(win_end == "2024-06-09") {next}
     
     # Convert window start and end to EW codes (for get_infodengue_data)
     win_start_ew <- date_to_ew(win_start)
@@ -76,25 +220,56 @@ run_moving_window <- function(root_dir_infodengue, root_dir_GT,
     delay_matrix <- window_data[[state]]
     delay_matrix[is.na(delay_matrix)] <- 0
     T_window <- nrow(delay_matrix)
-
-
+    
     
     # Arrange data for Stan
     if(model_name == "ARABMwGT"){
-      gt <- read.csv(file.path(root_dir_GT,win_end_ew, paste0(state,"_trends.csv")), 
-               stringsAsFactors = FALSE, skip = 2)[,c(1:3)] %>%
-        setNames(c("date", "dengue", "sintomas_dengue")) %>%
-        filter(date <= win_end & date >= win_start) %>%
-        mutate(
-          across(
-            .cols = -date,
-            .fns = ~ as.numeric(ifelse(. == "<1", "0.5", .))
-          )
-        ) %>% 
-        select("dengue", "sintomas_dengue") %>%
-        as.matrix()
-      stan_data <- c(list(T = T_window, D = D, Y = delay_matrix),
-                     list(K = 2, X =log(gt + 0.001)), hypers)
+      hist_start <- win_start - weeks(hist_len)       # hist start
+      hist_end   <- win_start - weeks(1)   
+      
+      hist_case <- readr::read_csv(file.path(path_source_denguetracker_infodengue,
+                                             win_end_ew,paste0(state,"_",as.character( win_end + weeks(1)),"_infodengue.csv")), 
+                                   show_col_types = FALSE) %>%
+        filter(ew_start >= hist_start & ew_start <= hist_end) %>%
+        select(sum_of_cases)
+      hist_case <- as.numeric(hist_case$sum_of_cases)
+      
+      gt_temp <- read.csv(file.path(root_dir_GT, win_end_ew, paste0(state, "_trends.csv")),
+                          stringsAsFactors = FALSE, skip = 2)[, c(1:2)] %>%
+        setNames(c("date", "dengue")) %>%
+        filter(date >= ealiest_date_for_smooth & date <= win_end) %>%
+        arrange(date) %>%
+        mutate(dengue = as.numeric(ifelse(dengue == "<1", "0.5", dengue)))
+      
+      gt <- zoo::rollmean(as.numeric(gt_temp$dengue),
+                          k = smooth_para_gt, align = "right", fill = NA)
+      
+      gt <- gt[-(1:(smooth_para_gt-1))] # remove NAs
+      
+      gt_dates <- gt_temp$date
+      gt_dates <- gt_dates[-(1:(smooth_para_gt-1))]
+      
+      dates_needed <- seq.Date(from = hist_start, to = win_end, by = "week")     
+      idx <- match(dates_needed, gt_dates)
+      gt_vec <- gt[idx]
+      
+      k <- smooth_para_gt
+      if (k > 1 && any(is.na(gt_vec[1:(k-1)]))) {                               
+        gt_vec <- gt_vec[-(1:(k-1))]
+        dates_needed <- dates_needed[-(1:(k-1))]
+      }
+      
+      expected_len <- hist_len + T_window
+      if (length(gt_vec) != expected_len) {                                       
+        stop(sprintf("Length of gt=%d, which is different from %d (historical length) + %d (length of run-off triangle)!", 
+                     length(gt_vec), hist_len, T_window))
+      }
+      
+      stan_data <- c(list(H = length(hist_case),T = T_window, D = D, 
+                          Y = delay_matrix, Y_hist = hist_case),
+                     list(K = 1, x = as.matrix(gt_vec), E = E,
+                          S=S, bases_s = bases_s[1:(length(hist_case)+T_window),]),
+                     hypers)
     }else{
       stan_data <- c(list(T = T_window, D = D, Y = delay_matrix), hypers)
     }
@@ -108,7 +283,7 @@ run_moving_window <- function(root_dir_infodengue, root_dir_GT,
       chains = chains,
       parallel_chains = chains,
       thin = thin,
-      refresh = 0,
+      refresh = 500,
       output_dir = posterior_draws_path
     )
     
@@ -170,5 +345,6 @@ extract_N_summary <- function(results, num_N = 8, model_name = "ARABM") {
   return(out_df)
 }
 
+#######################################################################
 
 
