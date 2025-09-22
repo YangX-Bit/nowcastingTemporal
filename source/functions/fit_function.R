bd2b4c946361a166c43ab7436c132de3b9405b81
Yang <yang.xiao@kaust.edu.sa>
2025-09-22 16:25:01 +0300

    SARI

--- source/functions/fit_function.R
+++ source/functions/fit_function.R
@@ -350,3 +350,141 @@ extract_N_summary <- function(results, num_N = 1, model_name = "ARABM") {
 #######################################################################
 
 
+nowcasting_moving_window_with_temporal <- function(data, scoreRange, case_true = NULL,
+                                                   start_date = NULL, start_date_triangular = NULL, predict_length = NULL,
+                                                   D = 20,
+                                                   methods = c("baseline", "proposed"),
+                                                   compiled_models,
+                                                   S, bases_s,
+                                                   E,
+                                                   iter_sampling = 2500, iter_warmup = 1000, refresh = 500,
+                                                   num_chains = 4, thin = 1,suppress_output = TRUE,
+                                                   posterior_draws_path = file.path(path_proj, "source", "models",
+                                                                                    "posterior_draws"), hypers = NULL
+){
+  if(is.null(case_true)){
+    stop("You must input true cases.")
+  }
+  
+  if (is.null(compiled_models) || !all(methods %in% names(compiled_models))) {
+    stop("You must provide compiled models matching 'methods'.")
+  }
+  
+  # get the date
+  if(is.null(start_date)){ start_date = rownames(data)[1] 
+  }else {
+    data <- data[rownames(data) >= start_date,]
+  } 
+  
+  # prepare data
+  data <- as.matrix(data)
+  scoreRange <- as.Date(scoreRange)
+  data_list <- slice_data(data, scoreRange, 
+                          start_date = start_date, window_day_length = predict_length)
+  scoreRange <- tail(scoreRange, length(data_list)) #remove invalid scoring date
+  # result list
+  model_fits <- list()
+  for (i in 1:length(scoreRange)) {
+    #What's "today"
+    now <- scoreRange[i]
+    # show the status
+    cat(paste("====================\nnow=",now,
+              " (",i,"/",length(scoreRange),")\n====================\n",sep=""))
+    
+    # prepare the data for Stan
+    data_use <- data_list[[i]]
+    data_trunc <- create_triangular_data(data_use, if_zero = F)
+    
+    # information for plot
+    model_fits[["case_true"]][[i]] <- case_true[rownames(case_true) 
+                                                %in% rownames(data_use), , drop = FALSE]
+    model_fits[["case_reported"]][[i]] <- extract_last_valid(data_trunc)
+    model_fits[["dates"]][[i]] <- as.Date(rownames(data_use))
+    
+    N_obs_local <- nrow(data_trunc) # num of obs
+    indices_data_trunc <- find_non_na_coords(data_trunc) # coordinates for non-NAs
+    data_trunc[is.na(data_trunc)] <- 0 # to avoid NAs in data
+    
+    # for historical
+    nrow_recent <- as.numeric((now - start_date_triangular)/7)
+    data_trunc_recent <- tail(data_trunc, nrow_recent)
+    N_obs_local_recent <- nrow(data_trunc_recent)
+    
+    data_trunc_hist <- head(data_trunc, N_obs_local - nrow_recent)
+    Y_hist <- data_trunc_hist[, c(ncol(data_trunc_hist))]
+    
+    if(nrow(data_trunc) <= D + 1){
+      warning("The number of rows of the input data is smaller than number of max delay D, which might cause inaccuracy." )
+    }
+    # return(stan_data_trunc)
+    # Fit models based on what is selected 
+    for (model_name in methods) {
+      compiled_model <- compiled_models[[model_name]]
+      if (is.null(compiled_model)) {
+        stop(paste("Model path for", model_name, "is not specified in model_paths."))
+      }
+      
+      # Fit the Stan model
+      if(model_name == "baseline"){
+        stan_data_trunc <- c(list(T = N_obs_local, D = D, Y = data_trunc), hypers)
+        
+        sampling_code <- function() {
+          compiled_model$sample(
+            data = stan_data_trunc,
+            iter_sampling = iter_sampling,
+            iter_warmup = iter_warmup,
+            chains = num_chains,
+            parallel_chains = num_chains,
+            refresh = refresh,
+            thin = thin,
+            output_dir = posterior_draws_path
+          )
+        }
+      }else if(model_name == "baseline2"){
+        stan_data_trunc <- c(list(T = N_obs_local_recent, D = D, Y = data_trunc_recent), hypers)
+        
+        sampling_code <- function(){
+          compiled_model$sample(
+            data = stan_data_trunc,
+            iter_sampling = iter_sampling,
+            iter_warmup = iter_warmup,
+            chains = num_chains,
+            parallel_chains = num_chains,
+            refresh = refresh,
+            thin = thin,
+            output_dir = posterior_draws_path
+          )
+        }
+      }else{
+        stan_data_trunc <- c(list(H = N_obs_local - N_obs_local_recent, 
+                                  T = N_obs_local_recent, D = D, 
+                                  Y = data_trunc_recent, Y_hist = Y_hist), hypers,
+                             list(S = S, bases_s = bases_s[1:N_obs_local,]), E = E)
+        
+        sampling_code <- function(){
+          compiled_model$sample(
+            data = stan_data_trunc,
+            iter_sampling = iter_sampling,
+            iter_warmup = iter_warmup,
+            chains = num_chains,
+            parallel_chains = num_chains,
+            refresh = refresh,
+            thin = thin,
+            output_dir = posterior_draws_path
+          )
+        }
+      }
+      
+      
+      if (suppress_output) {
+        fit <- suppressWarnings(suppressMessages(sampling_code()))
+      } else {
+        fit <- sampling_code()
+      }
+      
+      # Store the result
+      model_fits[[model_name]][[i]] <- fit
+    }
+  }
+  return(model_fits)
+}
